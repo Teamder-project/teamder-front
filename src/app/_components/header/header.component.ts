7f29833e7ba5010f9637e9eec039507714fa3db9
Maxime <maxime.deleruyelle@labomiformation.fr>
2021-06-09 19:03:51 +0200

    header mobile

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -18,21 +18,6 @@ export class HeaderComponent implements OnInit {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  // Close the dropdown if the user clicks outside of it
-    onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
 
   showDropdown(): void {
     document.getElementById("dropdown-content").style.display = "block";
