559fcbd1e7f92c9d06b5ab60203f48d7c5b36508
Mathyce D'Alessandro <dalessandro.mathyce@laposte.net>
2021-06-10 14:37:17 +0200

    dropdown mobile fait

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -8,16 +8,27 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    document.addEventListener('click', this.hideDropdownMobileOnOutsideClick.bind(this));
+  }
 
   ngOnInit(): void {
 
   }
 
-  myFunction(): void {
-    document.getElementById("myDropdown").classList.toggle("show");
+  dropdownMobile(): void {
+    document.getElementById("dropdown-content-mobile").classList.toggle("show");
   }
 
+  hideDropdownMobile(): void {
+    document.getElementById("dropdown-content-mobile").classList.remove("show");
+  }
+
+  hideDropdownMobileOnOutsideClick(event:any): void {
+    if (!document.getElementById("dropdown-mobile").contains(event.target)){
+      document.getElementById("dropdown-content-mobile").classList.remove("show");
+    }
+  }
 
   showDropdown(): void {
     document.getElementById("dropdown-content").style.display = "block";
