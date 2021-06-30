7ea5cb5b1d7a062c507b247253d87a3374e7420a
Maxime <maxime.deleruyelle@labomiformation.fr>
2021-06-30 17:31:48 +0200

    modif chat

--- src/app/models/chatMessageDto.ts
+++ src/app/models/chatMessageDto.ts
@@ -1,9 +1,15 @@
-export class ChatMessageDto {
-    user: string;
-    message: string;
+import { Gamer } from "./Gamer";
 
-    constructor(user: string, message: string) {
-        this.user = user;
+export class ChatMessageDto {
+    message: string;
+    sender : Gamer;
+    receiver : Gamer; 
+
+    constructor( message: string, sender : Gamer, receiver : Gamer) {
         this.message = message;
+        this.sender = sender;
+        this.receiver = receiver;
     }
+
+
 }
