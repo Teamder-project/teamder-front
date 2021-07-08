8df68cd5c83623bfdd0ac3f4cfdc830de78c0599
Miguelm2i <miguelbvo59@gmail.com>
2021-07-08 13:48:58 +0200

    First commit

--- src/app/models/FriendChat.ts
+++ src/app/models/FriendChat.ts
@@ -1,11 +1,13 @@
 import { Gamer } from "./Gamer";
 
-export class ChatMessageDto {
-    message: string;
+export class FriendChat {
+    id : number
+    message : string;
+    date : string;
     sender : Gamer;
     receiver : Gamer; 
 
-    constructor( message: string, sender : Gamer, receiver : Gamer) {
+    constructor(message: string, sender : Gamer, receiver : Gamer) {
         this.message = message;
         this.sender = sender;
         this.receiver = receiver;
