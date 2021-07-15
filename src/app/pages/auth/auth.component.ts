5037194040b280069ea7f911807304eeafa88f49
Mathyce D'Alessandro <dalessandro.mathyce@laposte.net>
2021-07-15 15:58:30 +0200

    Update auth.component.ts

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -28,8 +28,6 @@ export class AuthComponent implements OnInit {
     document.getElementById("login-content").style.display = "none";
     this.service.login(this.loginForm.value).subscribe(data => {
       localStorage.setItem("id", data);
-      document.getElementById("email")["value"] = "";
-      document.getElementById("password")["value"] = "";
       this.loginForm.value.email = "";
       this.loginForm.value.password = "";
       this.router.navigate(["index"]);
